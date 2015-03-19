ff7645f7740013bb8516951ec779a0ced47c37ef
seba1 <sebahoroszko@gmail.com>
2015-03-19 16:16:36 +0000

    update

--- main.py
+++ main.py
@@ -211,5 +211,7 @@ def displayScoreBoard(userNam):
 
 app.config['SECRET_KEY'] = 'thisismysecretkeywhichyouwillneverguesshahahahahahahaha'
 
+def main():
+    app.run(debug=True)
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    main()
